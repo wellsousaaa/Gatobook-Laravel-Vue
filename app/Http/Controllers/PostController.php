7f8daaa8cd9c1cef2b7d6197847ebfc652421b80
wellsousaaa <wendell25sousaferreira@gmail.com>
2022-12-21 14:18:24 -0300

    feat: vue template

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all()->toArray();
+        return array_reverse($posts);
+    }
+
+    public function store(Request $request)
+    {
+        $post = new Post([
+            'title' => request('title'),
+            'body' => request('body'),
+            'image' => request('image'),
+            'user_id' => request('user_id')
+        ]);
+        $post->save();
+
+        return response()->json('The post successfully added');
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return response()->json($post);
+    }
+
+    public function update($id, Request $request)
+    {
+        $post = Post::find($id);
+        $post->update($request->all());
+
+        return response()->json('The post successfully updated');
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+
+        return response()->json('The post successfully deleted');
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'title',
+        'body',
+        'image',
+        'user_id',
     ];
 }
